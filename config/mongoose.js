92a180bb898baa9136937a17c93cc92678096ee2
Saksham Gupta <sakshamgupta@Sakshams-MacBook-Air.local>
2023-07-31 13:23:56 +0530

    Change MongoDB URL to env

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-
+const url = process.env.MONGODB_URL;
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://saksham9312:Guitar%40123@swiftauth.frsmwje.mongodb.net/?retryWrites=true&w=majority');
+  await mongoose.connect(url);
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
